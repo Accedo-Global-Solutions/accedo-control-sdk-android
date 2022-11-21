d56cc23d55f82510c08553458747dcdfc40896a5
Yigit Ozen <yigit.ozen@accedo.tv>
2022-11-21 14:21:12 +0100

    Set min api to 21

--- control-sdk/src/main/java/tv/accedo/one/sdk/implementation/utils/DnsSelector.java
+++ control-sdk/src/main/java/tv/accedo/one/sdk/implementation/utils/DnsSelector.java
@@ -2,22 +2,38 @@ package tv.accedo.one.sdk.implementation.utils;
 
 import androidx.annotation.NonNull;
 
+import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import okhttp3.Dns;
 
+/**
+ * DNS selector for prioritizing IPV4 addresses over IPV6
+ */
 public class DnsSelector implements Dns {
 
     @NonNull
     @Override
     public List<InetAddress> lookup(@NonNull String hostname) throws UnknownHostException {
         // Prioritizing Inet4Addresses
-        return Dns.SYSTEM.lookup(hostname).stream().sorted(Comparator.comparing(inetAddress ->
-                inetAddress instanceof Inet6Address)).collect(Collectors.toList());
+        List<InetAddress> addresses = Dns.SYSTEM.lookup(hostname);
+        Collections.sort(addresses, (inetAddress1, inetAddress2) -> {
+                    if (inetAddress1 instanceof Inet4Address && inetAddress2 instanceof Inet6Address) {
+                        return -1;
+                    }
+
+                    if (inetAddress1 instanceof Inet6Address && inetAddress2 instanceof Inet4Address) {
+                        return 1;
+                    }
+
+                    return 0;
+                }
+
+        );
+        return addresses;
     }
 }

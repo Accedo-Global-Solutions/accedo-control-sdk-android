4ebf9347d8fbacd3defe6e283545e2f6baf49f31
Yigit Ozen <yigit.ozen@accedo.tv>
2023-10-16 12:44:52 +0200

    Added sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,38 @@ plugins {
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.jfrog.artifactory") version "5.1.10"
+    id("org.sonarqube") version "4.3.1.3277"
+}
+
+sonar {
+    properties {
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "accedo-global-solutions")
+        property("sonar.projectKey", "Accedo-Global-Solutions_accedo-control-sdk-android")
+        property("sonar.projectName", "Control SDK Android")
+        property("sonar.pullrequest.github.repository", "Accedo-Global-Solutions/accedo-control-sdk-android")
+        property("sonar.pullrequest.provider", "GitHub")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.language", "java")
+        property("sonar.log.level", "TRACE")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.tags", "control-sdk-android, android")
+        property("sonar.verbose", true)
+    }
+}
+
+subprojects {
+    sonar {
+        properties {
+            property("sonar.projectVersion", "#${this@subprojects.version}")
+            property("sonar.coverage.jacoco.xmlReportPaths", "${this@subprojects.buildDir}/reports/jacoco.xml")
+            property("sonar.java.binaries", "${this@subprojects.buildDir}/classes/java")
+            property("sonar.junit.reportPaths", "$buildDir/test-results/testDebugUnitTest")
+            property("sonar.sources", "${this@subprojects.projectDir}/src/main/java")
+            property("sonar.tests", "${this@subprojects.projectDir}/src/test/java")
+            property("sonar.android.lint.report", "${this@subprojects.buildDir}/reports/lint-results-debug.xml")
+        }
+    }
+}
 }
